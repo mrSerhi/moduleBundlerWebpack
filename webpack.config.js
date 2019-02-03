d6159d8b56b89fc655cb95ed446866a74df8129d
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-02-03 16:24:26 +0200

    webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebPack = require("webpack");
+
 module.exports = env => {
-  console.log(env);
+  // console.log(env); // env is a object { mode: development or production }
   return {
     output: {
       filename: "bundle.js"
     },
-    mode: env.mode
+    mode: env.mode,
+    plugins: [new HtmlWebpackPlugin(), new WebPack.ProgressPlugin()]
   };
 };
